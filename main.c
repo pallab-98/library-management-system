62432d2c59aed8177e7a031f20c0944bacfb8dd8
Pallab <mazharulislampallab98@gmail.com>
2026-08-18 06:48:21 +0600

    second commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "interface.h"
+
+int main()
+{
+    clearScreen();
+
+    printHeader();
+
+    printf("\n");
+    printf("        1. Registration\n");
+    printf("        2. Login\n");
+    printf("        3. Exit\n");
+
+    printf("\n        Enter Choice: ");
+
+    return 0;
+}
